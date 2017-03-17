ea901d0fb687821c0a0d777838c9e8926e39101f
corey-smith1 <corey_smith661@hotmail.com>
2017-03-17 14:42:55 +1000

    add prac 1

--- Prac 3/fun function.py
+++ Prac 3/fun function.py
@@ -1,21 +1,35 @@
+"""
+CP1404/CP5632 - Practical
+Pseudocode for temperature conversion
+"""
+
+MENU = "C - Convert Celsius to Fahrenheit\nF - Convert Fahrenheit to Celsius\nQ (for quit)"
+print(MENU)
+choice = input(">>> ").upper()
 
 
+def celsius_to_far():
+    global celsius, fahrenheit
+    celsius = float(input("Celsius: "))
+    fahrenheit = celsius * 9.0 / 5 + 32
+    print("Result: {:.2f} F".format(fahrenheit))
 
-def main():
-    menu = input('"C - Convert Celsius to Fahrenheit\nF - Convert Fahrenheit to Celsius\nQ (for quit)"')
 
-    def choose_d_or_f(menu):
+def far_to_celsius():
+    global fahrenheit, celsius
+    fahrenheit = float(input("fahrenheit: "))
+    celsius = 5 / 9 * (fahrenheit - 32)
+    print("Result: {:.2f} C".format(celsius))
+    pass
 
-        if choice == "C":
-            celsius = float(input("Celsius: "))
-            fahrenheit = celsius * 9.0 / 5 + 32
-            print("Result: {:.2f} F".format(fahrenheit))
-        elif choice == "F":
-            fahrenheit = float(input("fahrenheit: "))
-            celsius = 5 / 9 * (fahrenheit - 32)
-            print("Result: {:.2f} C".format(celsius))
-            pass
-        else:
-            print("Invalid option")
 
-main()
+while choice != "Q":
+    if choice == "C":
+        celsius_to_far()
+    elif choice == "F":
+        far_to_celsius()
+    else:
+        print("Invalid option")
+    print(MENU)
+    choice = input(">>> ").upper()
+print("Thank you.")
---
+++ learning things/ascii.py
@@ -0,0 +1,9 @@
+def main(x, y):
+    name = input("First Name: ")
+    print(name + str(x) + str(y))
+
+
+
+x = input("Middle Name: ")
+y = input("Last Name: ")
+main(x)
